bf4ca09edab39e28aca501164541f839d76b0adc
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-14 19:45:33 -0300

    main app

--- src/main/java/com/example/demo/MicroOrganizationServiceApplication.java
+++ src/main/java/com/example/demo/MicroOrganizationServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class MicroOrganizationServiceApplication {
 
 	public static void main(String[] args) {
