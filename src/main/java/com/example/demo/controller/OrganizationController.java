9e4f29ab521490ed403dae595aed46e5aec62e87
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-14 20:01:06 -0300

    order

--- src/main/java/com/example/demo/controller/OrganizationController.java
+++ src/main/java/com/example/demo/controller/OrganizationController.java
@@ -24,23 +24,29 @@ public class OrganizationController {
 
 
     @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
-    public ResponseEntity<Organization> getOrganization( @PathVariable("organizationId") String organizationId) {
+    public ResponseEntity<Organization> getOrganization( 
+    		@PathVariable("organizationId") String organizationId) {
         return ResponseEntity.ok(service.findById(organizationId));
     }
 
     @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
-    public void updateOrganization( @PathVariable("organizationId") String id, @RequestBody Organization organization) {
+    public void updateOrganization( 
+    		@PathVariable("organizationId") String id, 
+    		@RequestBody Organization organization) {
         service.update(organization);
     }
 
     @PostMapping
-    public ResponseEntity<Organization>  saveOrganization(@RequestBody Organization organization) {
+    public ResponseEntity<Organization>  saveOrganization(
+    		@RequestBody Organization organization) {
     	return ResponseEntity.ok(service.create(organization));
     }
 
     @RequestMapping(value="/{organizationId}",method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteOrganization( @PathVariable("id") String id,  @RequestBody Organization organization) {
+    public void deleteOrganization( 
+    		@PathVariable("id") String id,  
+    		@RequestBody Organization organization) {
         service.delete(organization);
     }
 }
