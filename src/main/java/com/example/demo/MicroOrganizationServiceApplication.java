d7e0cce5c416ae873d961662ed0c1ebae7e82a35
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-03 19:23:43 -0300

    bulding supplier

--- src/main/java/com/example/demo/MicroOrganizationServiceApplication.java
+++ src/main/java/com/example/demo/MicroOrganizationServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import java.util.function.Supplier;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RefreshScope
@@ -11,5 +14,21 @@ public class MicroOrganizationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroOrganizationServiceApplication.class, args);
 	}
+	
+	/**
+	 * Debería agregarse de forma automática
+	 * 
+	 * @return
+	 */
+	@Bean
+    public Supplier<String> output(){
+        return () -> {
+            // Add logic to generate the message if needed
+            String message = "Your generated message";
+            System.out.println("Sending message to Kafka topic: " + message);
+            return message;
+        };
+        
+    }
 
 }
--- src/main/java/com/example/demo/events/source/SimpleSourceBean.java
+++ src/main/java/com/example/demo/events/source/SimpleSourceBean.java
@@ -8,14 +8,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SimpleSourceBean {
 
-	@Bean
-    public Supplier<String> output(){
-        return () -> {
-            // Add logic to generate the message if needed
-            String message = "Your generated message";
-            System.out.println("Sending message to Kafka topic: " + message);
-            return message;
-        };
-        
-    }
+	
 }
