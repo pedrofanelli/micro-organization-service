4f0d92835e60dd4bb16a2e39c1aafaa5546d43b9
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-06 19:21:04 -0300

    examples and more info

--- src/main/java/com/example/demo/MicroOrganizationServiceApplication.java
+++ src/main/java/com/example/demo/MicroOrganizationServiceApplication.java
@@ -1,66 +1,20 @@
 package com.example.demo;
 
 
-import java.util.UUID;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RefreshScope
 public class MicroOrganizationServiceApplication {
-
-	@Autowired
-	StreamBridge streamBridge;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MicroOrganizationServiceApplication.class, args);
 	}
-	
-	/**
-	 * Debería agregarse de forma automática
-	 * 
-	 * NO! REVISAR STREAM BRIDGE!!
-	 * 
-	 * @return
-	 */
-	/*
-	@Bean
-    public Supplier<String> output(){
-        return () -> {
-            // Add logic to generate the message if needed
-            String message = "Your generated message";
-            System.out.println("Sending message to Kafka topic: " + message);
-            return message;
-        };
-        
-    }
-    */
-
-	/*
-	//Test supplier
-	@Bean
-	public Supplier<String> supplier() {
-		return () -> "random UUID: " + UUID.randomUUID();
-	}
-
-	@Bean
-	public Consumer<String> receive() {
-		return uuid -> {
-			//This binding is targeted for Kafka cluster 1.
-			streamBridge.send("foo-out-0", uuid);
-			//This binding is targeted for Kafka cluster 2.
-			//streamBridge.send("bar-out-0", uuid);
-		};
-	}
-	*/
 	
 	
 	/*
@@ -77,7 +31,19 @@ public class MicroOrganizationServiceApplication {
 		return s -> System.out.println("Data Consumed :: " + s.toUpperCase());
 	}
 	
+	/*
+	@Bean
+	public Supplier<String> producerBinding() {
+		return () -> {
+			try {
+				Thread.sleep(1500);
+			} catch(InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+			return "new data";
+		};
+	}
 	
-	
+	*/
 
 }
