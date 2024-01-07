75f95195fb5db254386bd9b7700c8c1af2fe6110
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-07 16:04:05 -0300

    building test on one micro

--- src/main/java/com/example/demo/MicroOrganizationServiceApplication.java
+++ src/main/java/com/example/demo/MicroOrganizationServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,21 +19,21 @@ public class MicroOrganizationServiceApplication {
 	}
 	
 	
-	/*
+	
 	@Bean
 	public Function<String,String> processorBinding() {
 		
 		return s -> s + " :: " + System.currentTimeMillis();
 		
 	}
-	*/
+	
 	
 	@Bean
 	public Consumer<String> consumerBinding() {
 		return s -> System.out.println("Data Consumed :: " + s.toUpperCase());
 	}
 	
-	/*
+	
 	@Bean
 	public Supplier<String> producerBinding() {
 		return () -> {
@@ -44,6 +46,6 @@ public class MicroOrganizationServiceApplication {
 		};
 	}
 	
-	*/
+	
 
 }
